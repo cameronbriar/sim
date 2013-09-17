5581b57b01fb7e6d82a37b36d8ba703fa121366f
Cameron Briar <cameronbriar@mail.fresnostate.edu>
2013-09-16 22:56:54 -0700

    fun, but still way off

--- sim.py
+++ sim.py
@@ -2,6 +2,7 @@
 
 from random import random
 import time
+import sys
 
 class Simulation(object):
     
@@ -38,17 +39,18 @@ class Simulation(object):
 
     def tick(self):
         self.time += self.step
-        time.sleep(5)
+        time.sleep(1)
 
     def simulate(self):
-        for event in self.occurs:
+        for event in self.occurs[:-1]:
             for variable in self.dynamics:
                 event(variable) 
+        self.occurs[-1]("")
 
     def run(self):
 
         # time starts here
-        while self.time <= self.time_end:
+        while self.time <= self.time_end or not self.time_end:
 
             # things happen
             self.simulate()
@@ -63,7 +65,7 @@ class Life(Simulation):
         Simulation.__init__(self)
         
         self.time     = 2013
-        self.time_end = 2038
+        self.time_end = 0
         
         print "Life has begun has begun."
         self.beginning_of_time = self.time
@@ -76,34 +78,56 @@ class Life(Simulation):
 
         self.ready    = 0
 
-        # life specific
-        self.population = 0
-
     def setup(self):
         print "Populating simulation..."
 
-        self.population = 1000
-        self.death_rate = 0.01
+        self.population = 100
+        self.next_birth = 1
+        self.death_rate = 0.05
+        self.birth_rate = 0.002
 
         for new_life in xrange(self.population):
-            life_form = {}
-            life_form['life'] = 'alive'
-            life_form['type'] = 'human'
-            life_form['id']   = new_life
-
+            life_form = self.new_life()
             self.dynamics.append(life_form)
 
         self.occurs.append(self.death)
+        self.occurs.append(self.life)
+        self.occurs.append(self.eotw)
+
+
+    def new_life(self):
+        life_form = {}
+        life_form['life'] = 'alive'
+        life_form['type'] = 'human'
+        life_form['id']   = self.next_birth
+        self.next_birth += 1
+        self.dynamics.append(life_form)
+        return life_form
+
 
     def death(self, type_of_life):
         if type_of_life.get('life'):
-            life = None if not random() > self.death_rate else type_of_life.get('life')
-            type_of_life['life'] = life
+            life = None if random() < self.death_rate else type_of_life.get('life')
 
-            if not life:
+            if not life and type_of_life['life']:
+                type_of_life['life'] = life
                 self.population -= 1
                 print "A death to %d" % type_of_life['id']
-                print "There are %d life forms still available." % self.population
+
+    def life(self, _):
+        life = 1 if random() < self.birth_rate else None
+        if life:
+            type_of_life = self.new_life()
+            self.population += 1
+            print "A life to %d" % type_of_life['id']
+
+    def eotw(self, _):
+        if self.population == 0:
+            print 'Game over. Time = %d' % self.time
+            sys.exit(1)
+        
+        print "There are %d life forms still available." % self.population
+
 
 
 l = Life()
